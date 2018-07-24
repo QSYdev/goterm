90272be350b8dfeec3fb16fc47dd704c3cd16226
Matias Pan <matias.pan26@gmail.com>
2018-07-24 17:51:39 -0300

    add internal cancellation for BLE

--- internal/ble/ble.go
+++ internal/ble/ble.go
@@ -1,6 +1,7 @@
 package ble
 
 import (
+	"sync"
 	"time"
 
 	"github.com/paypal/gatt"
@@ -18,6 +19,8 @@ const (
 	endPacket      = 0x01
 	payloadSize    = 0x13
 	packetInterval = 100
+
+	delay = 2
 )
 
 var (
@@ -26,23 +29,59 @@ var (
 	notifyUUID  = gatt.UUID16(0xCCCC)
 )
 
+type receiver struct {
+	mu      sync.RWMutex
+	lastRcv int64
+
+	ticker *time.Ticker
+	data   []byte
+}
+
+func (r *receiver) received(data []byte) {
+	r.mu.Lock()
+	r.lastRcv = time.Now().Unix() * 1000
+	if len(r.data) == 0 {
+		r.ticker = time.NewTicker(1 * time.Second)
+		go r.check()
+	}
+	r.mu.Unlock()
+	r.data = append(r.data, data...)
+}
+
+func (r *receiver) check() {
+	for n := range r.ticker.C {
+		r.mu.RLock()
+		if (n.Unix()*1000)-r.lastRcv < 500 {
+			r.mu.RUnlock()
+			continue
+		}
+		r.mu.RUnlock()
+		r.data = []byte{}
+		return
+	}
+}
+
+func (r *receiver) reset() {
+	r.data = []byte{}
+	r.ticker.Stop()
+}
+
 // NewService returns a new gatt service with the characteristics
 // of the IDK protocol.
 func NewService(client Client) *gatt.Service {
-	bytes := []byte{}
+	rcv := &receiver{data: []byte{}}
 	s := gatt.NewService(gatt.MustParseUUID(serviceUUID.String()))
 	s.AddCharacteristic(gatt.MustParseUUID(writeUUID.String())).HandleWriteFunc(
 		func(r gatt.Request, data []byte) (status byte) {
+			rcv.received(data[1:])
 			var s byte = gatt.StatusSuccess
 			if data[0] == continuePacket {
-				bytes = append(bytes, data[1:]...)
 				return s
 			}
-			bytes = append(bytes, data[1:]...)
-			if err := client.Write(bytes); err != nil {
+			if err := client.Write(rcv.data); err != nil {
 				s = gatt.StatusUnexpectedError
 			}
-			bytes = []byte{}
+			rcv.reset()
 			return s
 		})
 	s.AddCharacteristic(gatt.MustParseUUID(notifyUUID.String())).HandleNotifyFunc(
